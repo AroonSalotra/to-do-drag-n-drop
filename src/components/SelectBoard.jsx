7ed69d9099b91b72febfef9e7539d02869f102af
Aroon Salotra <aroonsalotra@gmail.com>
2023-02-09 17:39:10 +0000

    fix width on SelectBoard, now scales to 200px displa

--- src/components/SelectBoard.jsx
+++ src/components/SelectBoard.jsx
@@ -5,7 +5,7 @@ const SelectBoard = ({ redirect, title, access }) => {
     return (
         <Link to={redirect}>
 
-            <div className="bg-neutral-700 flex flex-col border-gra border-y-2 items-center pt-40 w-80 transition-all sm:hover:bg-neutral-500">
+            <div className="bg-neutral-700 flex flex-col border-gra border-y-2 items-center pt-40 w-screen sm:w-80 transition-all sm:hover:bg-neutral-500">
 
                 {access ? null : <AiFillLock className="text-8xl absolute -translate-y-28 text-neutral-600" />}
 
